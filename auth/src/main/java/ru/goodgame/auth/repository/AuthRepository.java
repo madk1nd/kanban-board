13535d884232cdee93e6f69dfc2c10f81150b164
Sergey Vlasov <madk1nd86@gmail.com>
2018-08-27 23:18:40 +0200

    add User model for queryDsl projections

--- auth/src/main/java/ru/goodgame/auth/repository/AuthRepository.java
+++ auth/src/main/java/ru/goodgame/auth/repository/AuthRepository.java
@@ -2,20 +2,24 @@ package ru.goodgame.auth.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
 import com.querydsl.sql.SQLQueryFactory;
 import org.postgresql.util.PGobject;
 import org.springframework.stereotype.Service;
 import ru.goodgame.auth.QUsers;
+import ru.goodgame.auth.model.User;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class AuthRepository implements IAuthRepository {
+
+    @Nonnull private static final QUsers USERS = QUsers.users;
 
     @Nonnull private final SQLQueryFactory factory;
     @Nonnull private final ObjectMapper mapper;
@@ -27,33 +31,31 @@ public class AuthRepository implements IAuthRepository {
 
     @Override
     public String getUser() {
-        List<Tuple> fetch = factory
+        User user = factory
                 .select(
-                    QUsers.users.id,
-                    QUsers.users.username,
-                    QUsers.users.password,
-                    QUsers.users.token
+                        Projections.constructor(
+                                User.class,
+                                USERS.id,
+                                USERS.username,
+                                USERS.password,
+                                USERS.token
+                        )
                 )
-                .from(QUsers.users)
-                .fetch();
+                .from(USERS)
+                .where(USERS.id.eq(UUID.fromString("a64515c3-14aa-4760-89b9-23f747387ec3")))
+                .fetchOne();
+//        a18275d1-14aa-4760-89b9-23f747387ec3
 
-        System.out.println(fetch);
-        PGobject pGobject = (PGobject) fetch.get(0).get(QUsers.users.token);
+        System.out.println(user);
+        System.out.println(user.getTokens().get("test"));
+//        PGobject pGobject = (PGobject) fetch.get(0).get(USERS.token);
 
-        System.out.println(pGobject);
-        Map<String, String> tokens = getTokens(pGobject);
-        System.out.println(tokens);
+//        System.out.println(pGobject);
+//        Map<String, String> tokens = getTokens(pGobject);
+//        System.out.println(tokens);
 
         return "test";
     }
 
-    private Map<String, String> getTokens(PGobject pGobject) {
-        String value = pGobject.getValue();
-        try {
-            return mapper.readValue(value, TypeFactory.defaultInstance().constructMapType(HashMap.class, String.class, String.class));
-        } catch (IOException e) {
-//            log.error
-            return new HashMap<>();
-        }
-    }
+
 }
